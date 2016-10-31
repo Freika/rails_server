f7abc5d5c3d13031bd9e8f31512168b82764f187
Freika <iamfrey@gmail.com>
2016-10-31 18:04:34 +0700

    Add daily crontab task for backups.

--- roles/backups/templates/backup.sh
+++
@@ -1,1 +0,0 @@
-pg_dump --no-owner {{ app_name }}_production > backups/`date +"%H:%M_%d_%m_%Y"`.sql -U {{ app_name }}
--- roles/backups/templates/remove_old_backups.sh
+++
@@ -1,1 +0,0 @@
-find /home/deploy/var/www/{{ app_name }}/shared/backups -type f -mtime +8 -exec rm -v {} \;
